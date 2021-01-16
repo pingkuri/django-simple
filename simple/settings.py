ae5cf2caade865b2fbfe3554e7c550d1961a86e3
pingkuri <zuzupython@gmail.com>
2021-01-17 01:01:58 +0900

    #

--- simple/settings.py
+++ simple/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7f64tgqb20li(tm=w*-v$udor$3l5a*uln6*ylnf@cy_ql4q04'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
